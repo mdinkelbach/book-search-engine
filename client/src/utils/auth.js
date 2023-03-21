4615d0c6d5a9ed290845aaf63196eb3e7074432d
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-20 21:54:47 -0500

    jose

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -5,7 +5,7 @@ import decode from 'jwt-decode';
 class AuthService {
   // get user data
   getProfile() {
-    return decode(this.getToken());
+    return decode(this.getToken()); 
   }
 
   // check if user's logged in
